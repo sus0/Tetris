ab8c49d793d64df82b02371fc57532753f31394f
sus0 <susie_su94@hotmail.com>
2015-02-26 23:32:47 -0500

    Add buttons on main screen.

---
+++ Assets/Scripts/InteractiveBtns.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteractiveBtns : MonoBehaviour {
+	// Update is called once per frame
+	public void StartButtonOnClick(){
+		Application.LoadLevel("MainScene");
+	}
+
+	public void ExitButtonOnClick(){
+		Application.Quit();
+	}
+}
