b3b5f4f040b4420b13b84adb0d68376b15adc210
sus0 <susie_su94@hotmail.com>
2015-03-02 23:45:44 -0500

    Add count down before game starts

---
+++ Assets/Scripts/CountDown.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CountDown : MonoBehaviour {
+	public GameObject one;
+	public GameObject two;
+	public GameObject three;
+
+	private Spawner m_SpawnerScript;
+	// Use this for initialization
+	void Start () 
+	{
+		m_SpawnerScript = GameObject.Find ("Spawn").GetComponent<Spawner>();
+		m_SpawnerScript.enabled = false;
+		GetReady();
+	}
+	
+	void GetReady ()
+	{
+		Instantiate(one, transform.position, Quaternion.identity);
+
+	}
+}
